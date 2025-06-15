a9f32f789def220c91f8a8e21b9268943db2db04
BuildTools <unconfigured@null.spigotmc.org>
2025-06-15 19:25:50 -0400

    Added important comment

--- src/main/java/net/blaz/pinkmines/ThePinkMines.java
+++ src/main/java/net/blaz/pinkmines/ThePinkMines.java
@@ -5,8 +5,8 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+// Very  important comment
 public class ThePinkMines implements ModInitializer {
-
 	public static final String MOD_ID = "the-pink-mines";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
